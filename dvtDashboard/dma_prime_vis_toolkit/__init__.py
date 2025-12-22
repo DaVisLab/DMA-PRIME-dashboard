231c9bc9ed12dcf7a994c74ba096d2a176764934
Dongyun <dongyuh@clemson.edu>
2025-12-22 13:11:09 -0500

    git init reset

--- dvtDashboard/dma_prime_vis_toolkit/__init__.py
+++ dvtDashboard/dma_prime_vis_toolkit/__init__.py
@@ -272,30 +272,30 @@ def create_app(development=False, dataDir=None):
                 'name': 'main',
                 'displayName': 'DMA-PRIME'
             },
-            {
-                'name': 'outbreak-detection',
-                'displayName': 'Outbreak Detection',
-                # 'active': True,
-                'html': 'outbreak-detection/outbreak-detection-panel_dy.html'
-            },
-            {
-                'name': 'riskindex-assessment',
-                'displayName': 'Risk Index Assessment',
-                # 'active': True,
-                'html': 'outbreak-detection/riskindex-analysis.html'
-            },
-            {
-                'name': 'outbreak-exploration',
-                'displayName': 'Test Interface',
-                'active': True,
-                'html': 'outbreak-detection/test-page.html'
-            },
             # {
-            #     'name': 'map',
-            #     'displayName': 'Map View',
-            #     'active': True,
-            #     'html': 'outbreak-detection/outbreak-detection-map-panel.html'
+            #     'name': 'outbreak-detection',
+            #     'displayName': 'Outbreak Detection',
+            #     # 'active': True,
+            #     'html': 'outbreak-detection/outbreak-detection-panel_dy.html'
             # },
+            # {
+            #     'name': 'riskindex-assessment',
+            #     'displayName': 'Risk Index Assessment',
+            #     # 'active': True,
+            #     'html': 'outbreak-detection/riskindex-analysis.html'
+            # },
+            # {
+            #     'name': 'outbreak-exploration',
+            #     'displayName': 'Test Interface',
+            #     'active': True,
+            #     'html': 'outbreak-detection/test-page.html'
+            # },
+            {
+                'name': 'map',
+                'displayName': 'Map View',
+                'active': True,
+                'html': 'outbreak-detection/outbreak-detection-map-panel.html'
+            },
         ]
         return render_template('outbreak-detection/outbreak-detection-base.html', panels=panels, metadata=metadata)
 
